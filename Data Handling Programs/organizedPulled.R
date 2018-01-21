b965f95b0191287ae8fbb922c6805b3c4f0f1afd
Prayag Gordy <prayaggordy@gmail.com>
2018-01-20 21:41:29 -0500

    Updated to include hockey

--- Data Handling Programs/organizedPulled.R
+++ Data Handling Programs/organizedPulled.R
@@ -6,6 +6,7 @@ require(stringr)
 
 load("basketball.RData")
 load("baseball.RData")
+load("hockey.RData")
 
 baseball <- baseball %>%
   select(Name, playerid, Team, Pos, WAR, height, weight) %>%
@@ -18,5 +19,13 @@ basketball <- basketball %>%
   mutate(Name = strsplit(Name, "\\", fixed = T)[[1]][1], z_score = (PER-mean(basketball$PER))/sd(basketball$PER)) %>%
   arrange(desc(z_score), height, weight)
 
+hockey <- hockey %>%
+  rename(Name = Player, Team = Tm, ExpectedPM = `E+/-`)
+hockey <- hockey %>% #we have to do it twice because of some weird dplyr stuff where the name change isn't immediately recognized or something like that
+  select(Name, playerid, Team, Pos, ExpectedPM, height, weight) %>%
+  mutate(Name = strsplit(Name, "\\", fixed = T)[[1]][1], z_score = (ExpectedPM-mean(hockey$ExpectedPM))/sd(hockey$ExpectedPM)) %>%
+  arrange(desc(z_score), height, weight)
+
 write_csv(baseball, "baseballClean.csv")
 write_csv(basketball, "basketballClean.csv")
+write_csv(hockey, "hockeyClean.csv")
